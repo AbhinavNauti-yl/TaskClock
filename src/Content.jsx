abb8c11aa89ac202fc8211f370c293839bfcd525
AbhinavNauti-yal <165134854+AbhinavNauti-yl@users.noreply.github.com>
2024-08-23 23:50:03 +0530

    Delete src directory

--- src/Content.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const content_context=createContext("Time")
--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import content_controler_slice from "../features/slices/content_controler_slice";
-
-
-export const store=configureStore({
-    reducer:{
-        content:content_controler_slice
-    }
-})
